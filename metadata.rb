b1f0688814ce98a9abfa7c203dc89d782cbccd52
Sebastian Grewe <sebgrewe@bigpoint.net>
2012-11-19 16:58:27 +0100

    using master metadata to fix merge conflict

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer        'Charles Duffy (PoweredByTippr)'
-maintainer_email  'charles@poweredbytippr.com'
-license           "Apache 2.0"
-description       "Configures iptables with Shorewall"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.2"
+maintainer		'Charles Duffy (PoweredByTippr)'
+maintainer_email	'charles@poweredbytippr.com'
+license			"Apache 2.0"
+description		"Configures iptables with Shorewall"
+long_description	IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version			"0.0.1"
 
 recipe "shorewall", "Configures and activates Shorewall firewall"
 
